a5107b9896ea6b15db6267d2b294cea4587be900
bis-uni-oldenburg <lars.heuer@uni-oldenburg.de>
2019-11-28 07:15:47 +0100

    Update set_data.php

--- gruppenraumbuchung/ajax_php/set_data.php
+++ gruppenraumbuchung/ajax_php/set_data.php
@@ -6,18 +6,34 @@ require_once("../classes/required_classes.php");
 $rr=new RoomReservation();
 
 $action=$_GET['action'];
+$data=$_GET['value'];
 
 switch($action)
 {
 	case "book_room":
 		$data=$_GET['value'];
+		
+		if(!preg_match("!^[0-9]{8}\,[0-9]{3,4}\,[0-9]{3,4}\,[0-9]{1,3}\,[^\ ,\'\’]+$!", $data))
+		{
+		    echo "error";
+		    exit;
+		}
+		
 		$data=explode(",", $data);
+
 		$response=$rr->bookRoom($data);
 		echo utf8_encode($response);
 		break;	
 		
 	case "confirm":
 		$data=$_GET['value'];
+		
+		if(!preg_match("!^[0-9]{8}\,[0-9]{3,4}\,[0-9]{1,3}$!", $data))
+		{
+		    echo "error";
+		    exit;
+		}
+		
 		$data=explode(",", $data);
 		$response=$rr->confirm($data);
 		echo utf8_encode($response);
@@ -26,6 +42,13 @@ switch($action)
 		
 	case "delete":
 		$data=$_GET['value'];
+		
+		if(!preg_match("!^[0-9]{8}\,[0-9]{3,4}\,[0-9]{1,3}$!", $data))
+		{
+		    echo "error";
+		    exit;
+		}
+		
 		$data=explode(",", $data);
 		$response=$rr->delete($data);
 		echo utf8_encode($response);
@@ -33,3 +56,4 @@ switch($action)
 
 }
 ?>
+
