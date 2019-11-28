0dab75750f797accda3bf9b7c940f0ef4e8ea9d8
bis-uni-oldenburg <lars.heuer@uni-oldenburg.de>
2019-11-28 07:19:46 +0100

    Update get_data.php

--- gruppenraumbuchung/ajax_php/get_data.php
+++ gruppenraumbuchung/ajax_php/get_data.php
@@ -11,18 +11,39 @@ switch($action)
 {
 	case "convert_time":
 		$time=$_GET['value'];
+		
+		if(!preg_match("!^[0-9]{3,4}$!", $time))
+		{
+		    echo $time;
+		    exit;
+		}
+		
 		$time=$rr->convertTimeFromDec($time);
 		echo utf8_encode($time);
 		break;	
 	
 	case "convert_date":
 		$date=$_GET['value'];
-		$date=Time::YmdToGermanDate($date);
+		
+		if(!preg_match("!^[0-9]{8}$!", $date))
+		{
+		    echo $date;
+		    exit;
+		}
+		
+		$date=@Time::YmdToGermanDate($date);
 		echo utf8_encode($date);
 		break;	
 		
 	case "get_end_times":
 		$dtr=$_GET['value'];
+		
+		if(!preg_match("!^[0-9]{8}\,[0-9]{3,4}\,[0-9]{1,3}$!", $dtr))
+		{
+		    echo $dtr;
+		    exit;
+		}
+		
 		$p=explode(",", $dtr);
 		$times=$rr->getPossibleEndTimes($p[0], $p[1], $p[2]);
 		echo utf8_encode($times);
@@ -30,12 +51,26 @@ switch($action)
 		
 	case "get_room_title":
 		$room_number=$_GET['value'];
+		
+		if(!preg_match("!^[0-9]{1,3}$!", $room_number))
+		{
+		    echo $room_number;
+		    exit;
+		}
+		
 		$room_title=$rr->getRoomTitleByNumber($room_number);
 		echo utf8_encode($room_title);
 		break;
 		
 	case "day_quota_exhausted":
 		$data=$_GET['value'];
+		
+		if(!preg_match("!^[^\ ,\'\’]+\,[0-9]{8}$!", $data))
+		{
+		    echo 1;
+		    exit;
+		}
+		
 		$d=explode(",", $data);
 		$user=$d[0];
 		$date=$d[1];
